c2db72dd06d237df352d7f3b2a2e4d9fed60b329
Atik2788 <ashanto653.2@gmail.com>
2022-11-10 17:57:16 +0600

    after jwt token

--- src/Sheared/Login/Login.js
+++ src/Sheared/Login/Login.js
@@ -49,6 +49,20 @@ const Login = () => {
       
       console.log(currentUser)
       
+      // get jwt token 
+      fetch('https://meet-your-trainer-server-atik2788.vercel.app/jwt', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(currentUser)
+      })
+      .then(res=>res.json())
+      .then(data => {
+        console.log(data);
+        localStorage.setItem('trainer-token', data.token);
+        navigate(from, {replace: true})
+      })
 
       // form.reset();      
     })
